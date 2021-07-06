33f9da58be9293e3c6cef3362da66cc72ed258a6
Sara Taylor <sara.swifti@gmail.com>
2021-07-06 17:27:22 -0400

    cleaned up project page and favocon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <nav className=" nav navbar navbar-expand-lg sticky-top">
             
-                <li className="navbar-brand" to="../pages/About.js"><h1>Sara Taylor's Profile</h1><p>Made with REACT</p></li>
+                <li className="navbar-brand" to="../pages/About.js"><h1>Sara Taylor's Profile</h1><p>Made with React</p></li>
                 <ul className="navcont navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                         <Link className="nav-link active"  to="/">About</Link>
--- src/pages/About.js
+++ src/pages/About.js
@@ -22,7 +22,7 @@ function About() {
           </div>
         </div>
         <div className="col">
-          <p>Hello, my name is Sara Taylor. I never saw myself as a coder, however those who are close to me and understand my creative technical skill sets encouraged me to take a deeper look at web design.
+          <p>Hello, my name is Sara Taylor. I never saw myself as a coder, however those who are close to me and understand my creative technical skill sets, encouraged me to take a deeper look at web design.
             Sooo, here I am making the leap and loving it.  </p>
           <br></br>
           <p> I will be updating the projects on my portfolio page as I complete them. Thank you for visiting. </p>
